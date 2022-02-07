4302d1166d51665cdca0ef6cd55eb2ac5ad49331
Vanyo Ivanov <vanyoivanov98@gmail.com>
2022-02-07 15:23:02 +0200

    refactor: Header.tsx using material ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
-import styles from "../../styles/Header.module.css";
+import { Box, Typography } from "@mui/material";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className={styles.title}>
-        <span>Ani</span>List
-      </h1>
-      <p style={{ textAlign: "center" }}>Current most popular anime</p>
-    </div>
+    <Box sx={{ margin: "auto" }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+      >
+        <Typography
+          sx={{ fontSize: "4rem", color: "#0070f3" }}
+          variant="h1"
+          component="h1"
+        >
+          Ani
+        </Typography>
+        <Typography sx={{ fontSize: "4rem" }} variant="h1" component="h1">
+          List
+        </Typography>
+      </Box>
+      <Typography
+        sx={{ textAlign: "center", padding: "1rem" }}
+        variant="body1"
+        component="p"
+      >
+        Current most popular anime
+      </Typography>
+    </Box>
   );
 };
 
